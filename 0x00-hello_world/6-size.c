91ed4b8150db78fc4be8a208534d79a150c2db32
meryem el yatim <meryemelyatim7890@gmail.com>
2023-09-07 15:44:01 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various types
+ * return: 0 (success)
+ */
+int main(void)
+{
+printf("Size of a char: %lu byte(s)", sizeof(char));
+printf("Size of an int: %lu byte(s)", sizeof(int));
+printf("Size of a long int: %lu byte(s)", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+printf("Size of a float: %lu byte(s)", sizeof(float));
+return (0);
