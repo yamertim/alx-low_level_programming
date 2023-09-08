d03c4203ab57dfb44149d66d4a2cf9d6f50f1f23
meryem el yatim <meryemelyatim7890@gmail.com>
2023-09-08 16:38:15 -0700

    alpha9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,19 +8,13 @@
 
 int main(void)
 {
-	int n;
+	int i;
 
-	for (n = 0 ; n < 16 ; n++)
+	for (i = 0; i < 10; i++)
 	{
-		if (n < 10)
-		{
-			putchar('0' + n);
-		}
-		else
-		{
-			putchar(87 + n);
-		}
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
 	}
-	putchar('\n');
 	return (0);
 }
