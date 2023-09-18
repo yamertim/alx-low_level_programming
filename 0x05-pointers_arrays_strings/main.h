f90118e29f3590ea23a82a72d6e3007cba31583c
meryem el yatim <meryemelyatim7890@gmail.com>
2023-09-18 15:50:36 -0700

    puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,20 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
-#include <unistd.h>
 
-int _putchar(char c);
+/**
+* void prototypes()
+*char prototypes()
+* int prototypes()
+*/
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
+void swap_int(int *a, int *b);
+void swap_char(char *a, char *b);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int _strlen(char *s);
+int _atoi(char *s);
 
-#endif
-
+#endif /*MAIN_H*/
